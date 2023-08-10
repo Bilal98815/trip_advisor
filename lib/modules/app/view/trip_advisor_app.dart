a4bb95faf2288a5a611dceb635388d99fbf44aef
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-08-10 21:04:51 +0500

    created app folder to contain root of the app

---
+++ lib/modules/app/view/trip_advisor_app.dart
@@ -0,0 +1,82 @@
+part of 'view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => LoginBloc(
+            loginRepository:
+                LoginRepositoryImp(loginAuthService: LoginAuthService()),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => SignupBloc(
+            signUpRepository: SignUpRepositoryImp(
+              signupAuthService: SignupAuthService(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => UserDataBloc(
+            userDataRepository: UserDataRepositoryImp(
+              userDataAuth: UserDataAuth(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => ForgotPasswordBloc(
+            forgotPasswordRepository: ForgotPasswordRepositoryImp(
+              forgotPasswordAuth: ForgotPasswordAuth(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => LocationDataBloc(
+            locationDataRepository: LocationDataRepositoryImp(
+              locationDataAuth: LocationDataAuth(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => PlanTripBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ReviewBloc(),
+        ),
+        BlocProvider(
+          create: (context) => AccountBloc(
+            accountRepository: AccountRepositoryImp(
+              accountAuth: AccountAuth(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => ProfileBloc(
+            profileRepository: ProfileRepositoryImp(
+              profileAuth: ProfileAuth(),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => EditProfileBloc(
+            editProfileRepository:
+                EditProfileRepositoryImp(editProfileAuth: EditProfileAuth()),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+      ),
+    );
+  }
+}
