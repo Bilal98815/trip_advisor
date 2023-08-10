259149d097746f76a3d3da3a9e7f68a90b1c8a50
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-08-10 11:01:45 +0500

    created enum for Tabs

--- lib/modules/bottom_bar/presentation/models/icon_tab.dart
+++ lib/modules/bottom_bar/presentation/models/icon_tab.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:trip_advisor/modules/account/presentation/view/account_view.dart';
+import 'package:trip_advisor/modules/explore/presentation/view/explore_view.dart';
+import 'package:trip_advisor/modules/plan_trip/presentation/view/plan_trip_view.dart';
+import 'package:trip_advisor/modules/review/presentation/view/review_view.dart';
+import 'package:trip_advisor/modules/search/presentation/search_view.dart';
+
+enum Tabs { explore, search, plan, review, account }
+
+extension TabsX on Tabs {
+  IconTab get iconTab {
+    switch (this) {
+      case Tabs.explore:
+        return IconTab(
+          label: 'Explore',
+          icon: Icons.home_rounded,
+          route: ExploreView.route(),
+        );
+      case Tabs.search:
+        return IconTab(
+          label: 'Search',
+          icon: Icons.search,
+          route: SearchView.route(),
+        );
+      case Tabs.plan:
+        return IconTab(
+          label: 'Plan',
+          icon: Icons.favorite,
+          route: PlanTripView.route(),
+        );
+      case Tabs.review:
+        return IconTab(
+          label: 'Review',
+          icon: Icons.create,
+          route: ReviewView.route(),
+        );
+      case Tabs.account:
+        return IconTab(
+          label: 'Account',
+          icon: Icons.home_rounded,
+          route: AccountView.route(),
+        );
+      default:
+        return IconTab(
+          label: 'Explore',
+          icon: Icons.home_rounded,
+          route: ExploreView.route(),
+        );
+    }
+  }
+}
 
 class IconTab {
   const IconTab({
-    required this.index,
     required this.label,
     required this.icon,
     required this.route,
   });
 
-  final int index;
   final String label;
   final IconData icon;
   final String route;
--- lib/modules/bottom_bar/presentation/view/bottom_bar_view.dart
+++ lib/modules/bottom_bar/presentation/view/bottom_bar_view.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:trip_advisor/modules/account/presentation/view/account_view.dart';
 import 'package:trip_advisor/modules/bottom_bar/presentation/models/icon_tab.dart';
-import 'package:trip_advisor/modules/explore/presentation/view/explore_view.dart';
-import 'package:trip_advisor/modules/plan_trip/presentation/view/plan_trip_view.dart';
-import 'package:trip_advisor/modules/review/presentation/view/review_view.dart';
-import 'package:trip_advisor/modules/search/presentation/search_view.dart';
 
 class BottomBarView extends StatelessWidget {
   BottomBarView({
@@ -17,42 +12,11 @@ class BottomBarView extends StatelessWidget {
   final Widget child;
   final String currentRoute;
 
-  final List<IconTab> tabs = [
-    IconTab(
-      index: 0,
-      label: 'Explore',
-      icon: Icons.home_rounded,
-      route: ExploreView.route(),
-    ),
-    IconTab(
-      index: 1,
-      label: 'Search',
-      icon: Icons.search,
-      route: SearchView.route(),
-    ),
-    IconTab(
-      index: 2,
-      label: 'Plan',
-      icon: Icons.favorite,
-      route: PlanTripView.route(),
-    ),
-    IconTab(
-      index: 3,
-      label: 'Review',
-      icon: Icons.create,
-      route: ReviewView.route(),
-    ),
-    IconTab(
-      index: 4,
-      label: 'Account',
-      icon: Icons.home_rounded,
-      route: AccountView.route(),
-    ),
-  ];
+  final List<Tabs> tabs = Tabs.values.toList();
 
   int getCurrentIndex() {
     for (int i = 0; i < tabs.length; i++) {
-      if (tabs[i].route == currentRoute) {
+      if (tabs[i].iconTab.route == currentRoute) {
         return i;
       }
     }
@@ -69,18 +33,18 @@ class BottomBarView extends StatelessWidget {
           backgroundColor: Colors.black87,
           type: BottomNavigationBarType.fixed,
           currentIndex: currentIndex,
-          onTap: (val) => context.go(tabs[val].route),
+          onTap: (val) => context.go(tabs[val].iconTab.route),
           selectedItemColor: Colors.white,
           unselectedItemColor: Colors.grey.withOpacity(0.7),
           items: List.generate(
             tabs.length,
             (index) => BottomNavigationBarItem(
               icon: NavBarIcon(
-                iconTab: tabs[index],
+                iconTab: tabs[index].iconTab,
                 isActive: currentIndex == index,
                 size: size,
               ),
-              label: tabs[index].label,
+              label: tabs[index].iconTab.label,
             ),
           ),
         );
