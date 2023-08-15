47783d5b8ed58eaaf9cf59c7ec87973d3339f0c6
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-08-15 14:06:14 +0500

    moving router to app

--- lib/modules/app/view/view.dart
+++ lib/modules/app/view/view.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:trip_advisor/common/router/router.dart';
-
 import 'package:trip_advisor/modules/account/account.dart';
+import 'package:trip_advisor/modules/app/router/router.dart';
 import 'package:trip_advisor/modules/login/login.dart';
 
 part 'trip_advisor_app.dart';
