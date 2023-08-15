4b0426801424f9876f9714099919365742f51e6b
hadiyaaamir <hadiyaaamir222@gmail.com>
2023-08-15 13:16:18 +0500

    splash barrels

--- lib/modules/splash/presentation/view/splash_view.dart
+++ lib/modules/splash/presentation/view/splash_view.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+part of 'view.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
---
+++ lib/modules/splash/presentation/view/view.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+part 'splash_view.dart';
