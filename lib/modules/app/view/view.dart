ffcd4bf589b55128844f01387f72423d08481e03
bilal <lalib0311@gmail.com>
2023-08-16 14:50:40 +0500

    - changed the path of profile bloc file in widgets.dart file

--- lib/modules/app/view/view.dart
+++ lib/modules/app/view/view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:localization/localization.dart';
-
 import 'package:trip_advisor/modules/account/account.dart';
 import 'package:trip_advisor/modules/app/router/router.dart';
 import 'package:trip_advisor/modules/login/login.dart';
+
+import '../../profile/data/data.dart';
+import '../../profile/domain/repository/repository.dart';
+import '../../profile/presentation/bloc/profile_bloc.dart';
 
 part 'trip_advisor_app.dart';
 part 'trip_advisor_app_view.dart';
