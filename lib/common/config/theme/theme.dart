9bf1d5f428843613915e319d04f51073b736276d
bilal <lalib0311@gmail.com>
2023-08-18 17:58:13 +0500

    - made light mode except text themes

--- lib/common/config/theme/theme.dart
+++ lib/common/config/theme/theme.dart
@@ -4,7 +4,77 @@ import 'package:flutter/material.dart';
 
 class TripAdvisorTheme {
   static ThemeData get light {
-    return ThemeData(useMaterial3: true);
+    return ThemeData(
+      useMaterial3: true,
+      appBarTheme: const AppBarTheme(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        titleTextStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      hintColor: Colors.grey,
+      disabledColor: Colors.grey.shade700,
+      primaryTextTheme: const TextTheme(
+        // used in titles on Account, review, plan trips screen
+        titleLarge: TextStyle(
+          color: Colors.black,
+          fontSize: 32,
+          fontWeight: FontWeight.w900,
+        ),
+        displayMedium: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+
+        // used in imageContainer widget in review screen
+        displayLarge: TextStyle(
+          color: Colors.white,
+          fontSize: 40,
+          fontWeight: FontWeight.w900,
+          overflow: TextOverflow.clip,
+        ),
+
+        // used for version, device id in account screen
+        displaySmall: TextStyle(
+          color: Colors.black,
+          fontSize: 11,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+      primaryColor: Colors.white,
+      primaryColorLight: Colors.white,
+      colorScheme: const ColorScheme.light(
+        secondaryContainer: Colors.white,
+
+        // used in bottom bar above active item
+        tertiary: Colors.lightGreenAccent,
+
+        // used in trips tab in plan_trip screen
+        tertiaryContainer: Color(0xFF235347),
+
+        // used when there is error in validation in text field
+        onError: Colors.red,
+
+        // used in text field when there is no error
+        onPrimary: Colors.black,
+      ),
+      dividerTheme: const DividerThemeData(
+        color: Colors.grey,
+        thickness: 0.4,
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey.withOpacity(0.7),
+      ),
+    );
   }
 
   static ThemeData get dark {
@@ -51,20 +121,27 @@ class TripAdvisorTheme {
           fontWeight: FontWeight.w300,
         ),
       ),
+
       // main bg color of app
       primaryColor: Colors.black87,
+
       // bg color of buttons
       primaryColorDark: Colors.black12,
+
       // border color of buttons when bg color is black
       primaryColorLight: Colors.white,
       colorScheme: const ColorScheme.dark(
         secondaryContainer: Colors.black12,
+
         // used in bottom bar above active item
         tertiary: Colors.lightGreenAccent,
+
         // used in trips tab in plan_trip screen
         tertiaryContainer: Color(0xFF235347),
+
         // used when there is error in validation in text field
         onError: Colors.red,
+
         // used in text field when there is no error
         onPrimary: Colors.white,
       ),
